e24c19b48776948308fee8812c3ef7345bf62d79
Hamza Ilyas <hamza24061@gmail.com>
2021-11-04 01:15:24 +0500

    function change to callback function

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function getWeather() {
   let cityName = document.getElementById("cityName").value;
   let URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=8bd6abdc20db1f463412a5c9df7dc7d7&units=metric`;
 
-  axios.get(URL).then(function (response) {
+  axios.get(URL).then((response) => {
     console.log(response);
     console.log(response.data.main.temp);
     let temp = response.data.main.temp;
