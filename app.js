6743290682224be5441dc90fba1d20684a72236f
Hamza Ilyas <hamza24061@gmail.com>
2021-11-04 01:10:35 +0500

    Code Change

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 function getWeather() {
-  const cityName = document.getElementById("cityName").value;
-  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=8bd6abdc20db1f463412a5c9df7dc7d7&units=metric`;
+  let cityName = document.getElementById("cityName").value;
+  let URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=8bd6abdc20db1f463412a5c9df7dc7d7&units=metric`;
 
-axios.get(URL).then(function (response) {
+  axios.get(URL).then(function (response) {
     console.log(response);
     console.log(response.data.main.temp);
-    var temp = response.data.main.temp;
-    document.getElementById("result").innerHTML = `The Current Temprature of ${cityName} is ${temp} <span>&#176;</span>C`;
-  })
+    let temp = response.data.main.temp;
+    const result = document.getElementById("result");
+    result.innerHTML = `The Current Temprature of ${cityName} is ${temp} <span>&#176;</span>C`;
+  });
 }
-
